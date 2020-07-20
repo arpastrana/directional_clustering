4018b5da9754c7a3991adb40a37ab2b88b6ecbc8
Rafael Pastrana <pastrana@arch.ethz.ch>
2020-07-19 23:42:45 -0500

    Implemented cosine similarity

--- src/directional_clustering/geometry/vectors.py
+++ src/directional_clustering/geometry/vectors.py
@@ -1,9 +1,12 @@
+from compas.geometry import dot_vectors
+from compas.geometry import length_vector
 from compas.geometry import rotate_points
 
 
 __all__ = [
     "vector_from_angle",
-    "vectors_from_angles"
+    "vectors_from_angles",
+    "cosine_similarity"
     ]
 
 
@@ -22,6 +25,11 @@ def vectors_from_angles(angles, base_vector):
         vectors[fkey] = vector_from_angle(angle, base_vector)
 
     return vectors
+
+
+def cosine_similarity(u, v):
+
+    return dot_vectors(u, v) / (length_vector(u) * length_vector(v))
 
 
 if __name__ == "__main__":
