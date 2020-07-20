5c71eeac7c1f177d6e1f6fa02d25a4c30adc7653
Rafael Pastrana <pastrana@arch.ethz.ch>
2020-07-19 23:41:27 -0500

    Implemented laplacian smoothing

--- src/directional_clustering/geometry/angles.py
+++ src/directional_clustering/geometry/angles.py
@@ -16,7 +16,8 @@ __all__ = [
     "raw_cos_angle",
     "signed_angle",
     "clockwise",
-    "smoothed_angles"
+    "smoothed_angles",
+    "laplacian_smoothed"
 ]
 
 
@@ -54,6 +55,26 @@ def smoothed_angles(mesh, angles, smooth_iters):
             smoothed[fkey] = averaged_angles[fkey]
 
     return smoothed
+
+
+def laplacian_smoothed(mesh, data, iters, damping=0.5):
+    
+    data = {k: v for k, v in data.items()}
+
+    for i in range(iters):
+
+        iter_smoothed = {}
+
+        for fkey in mesh.faces():
+            f_data = data[fkey]
+            nbr_data = array([data[key] for key in mesh.face_neighbors(fkey)])
+            nbr_data = mean(nbr_data, axis=0)
+            iter_smoothed[fkey] = f_data + (1 - damping) * (nbr_data - f_data)
+
+        for fkey in mesh.faces():
+            data[fkey] = iter_smoothed[fkey]
+
+    return data
 
 
 if __name__ == "__main__":
