41fec338230bcfb273dfdac74eb91c7a9a44217c
Rafael Pastrana <pastrana@arch.ethz.ch>
2020-07-20 23:51:27 -0500

    Implemented 2d, 3d plots, deviations, labels, cosim colors

--- scripts/plot_vectors_xy.py
+++ scripts/plot_vectors_xy.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from math import acos
 from math import degrees
 from math import fabs
 
@@ -51,9 +52,11 @@ tags = [
     "custom_2"
     ]
 
-HERE = "../data/json_files/four_point_slab"
+HERE = "../data/json_files/two_point_wall"  # leonhardt
+HERE = "../data/json_files/wall_with_hole"  # schlaich
 
-tag = "m_1"
+
+tag = "n_2"
 x_lim = -10.0  # faces stay if x coord of their centroid is larger than x_lim
 
 # =============================================================================
@@ -89,36 +92,30 @@ for fkey in mesh.faces():
     mesh.face_attribute(fkey, tag, vector)
 
 # =============================================================================
-# Process PS vectors
+# Align vectors
 # =============================================================================
 
 align = True
-# align_ref = [0.0, 1.0, 0.0]
-align_ref = [1.0, 0.0, 0.0]
-normalize = False
-
-rescale = False
-vec_scale = 1.0  # for rescaling, max length
-
-# =============================================================================
-# Align vectors
-# =============================================================================
+align_ref = [1.0, 0.0, 0.0]  # global x
 
 vectors = {}
 for fkey in mesh.faces():
     vector = mesh.face_attribute(fkey, tag) 
+    
     if align:
         if dot_vectors(align_ref, vector) < 0:
             vector = scale_vector(vector, -1)
-    if normalize:
-        vector = normalize_vector(vector)
+
     vectors[fkey] = vector
 
 # =============================================================================
-# Scale and normalize vectors
+# Plot vectors 2d
 # =============================================================================
 
 plot_vectors_2d = False
+normalize = True
+rescale = False
+vec_scale = 1.0  # for rescaling, max length
 
 if plot_vectors_2d:
     lengths = [length_vector(vec) for k, vec in vectors.items()]
@@ -129,6 +126,9 @@ if plot_vectors_2d:
     y = np.zeros(mesh.number_of_faces())
 
     for fkey, vector in vectors.items():
+
+        if normalize:
+            vectros = normalize_vector(vector)
 
         if rescale:
             length = length_vector(vector)
@@ -147,67 +147,86 @@ if plot_vectors_2d:
     plt.show()
 
 # =============================================================================
-# Cosine similarity
+# Store original vectors before smoothing and normalization
 # =============================================================================
 
-ref_cosim = [0.0, 1.0, 0.0]
-# ref_cosim = [1.0, 0.0, 0.0]
-
-cosim = {}
-for fkey, vector in vectors.items():
-    # cosim[fkey] = cosine_similarity(ref_cosim, vector)
-    cosim[fkey] = normalize_vector(vector)  # cluster based on normalized and aligned vectors
+raw_values = np.zeros((mesh.number_of_faces(), 3))
+for fkey, vec in vectors.items():
+    raw_values[fkey,:] = vec
 
 # =============================================================================
 # Smoothen vectors
 # =============================================================================
 
-smooth_iters = 0
+smooth_iters = 1
 damping = 0.5
 
 if smooth_iters:
-    cosim = laplacian_smoothed(mesh, cosim, smooth_iters, damping)
-
-# min_cosim = min(cosim.values())
-# max_cosim = max(cosim.values())
+    vectors = laplacian_smoothed(mesh, vectors, smooth_iters, damping)
 
 # =============================================================================
-# Print stuff
+# Cosine similarity
 # =============================================================================
 
-# print("max cosim: {}".format(max_cosim))
-# print("min cosim: {}".format(min_cosim))
+ref_cosim = [0.0, 1.0, 0.0]  # global y - for full systems
+# ref_cosim = [1.0, 0.0, 0.0]  # global x - for symmetric partition
+
+cosim = np.zeros(mesh.number_of_faces())
+for fkey, vec in vectors.items():
+    cs = cosine_similarity(ref_cosim, vec)
+
+    # helps clustering in a good way
+    # square cosim values and keep sign
+    # s = np.sign(cs)
+    # cs = s * (cs ** 2)
+
+    cosim[fkey] = cs
 
 # =============================================================================
-# Dict to array
+# Plot cosim as heights
 # =============================================================================
 
-# values = np.zeros((mesh.number_of_faces(), 1))
-# for fkey, cs in cosim.items():
-#     values[fkey] = cs
+plot_points_3d = False
 
-values = np.zeros((mesh.number_of_faces(), 3))
-for fkey, cs in cosim.items():
-    values[fkey,:] = normalize_vector(cs)
+if plot_points_3d:
 
-# =============================================================================
-# # Plot 3d - Data as Z
-# # =============================================================================
-
-# D = np.zeros((mesh.number_of_faces(), 3))
-
-# for fkey in mesh.faces():
-#     x, y, z = mesh.face_centroid(fkey)
+    # plot in 3d
+    D = np.zeros((mesh.number_of_faces(), 3))
+    for fkey in mesh.faces():
+        x, y, z = mesh.face_centroid(fkey)
+        
+        D[fkey, 0] = x
+        D[fkey, 1] = y
+        D[fkey, 2] = cosim[fkey]
     
-#     D[fkey, 0] = x
-#     D[fkey, 1] = y
-#     D[fkey, 2] = values[fkey, :]
+    fig = plt.figure(figsize=(16, 9))
+    ax = plt.axes(projection="3d")
+
+    x = D[:,0]
+    y = D[:,1]
+    z = D[:,2]
+
+    ax.scatter3D(x, y, z, c=z, cmap="jet")
+    ax.view_init(70, -65)
+
+    plt.show()
+
+# =============================================================================
+# Normalize vectors
+# =============================================================================
+
+smoothed_values = np.zeros((mesh.number_of_faces(), 3))
+values = np.zeros((mesh.number_of_faces(), 3))
+
+for fkey, vec in vectors.items():
+    smoothed_values[fkey,:] = vec
+    values[fkey,:] = normalize_vector(vec)
 
 # =============================================================================
 # Kmeans Clustering
 # =============================================================================
 
-n_clusters = 5
+n_clusters = 6
 do_kmeans = True
 
 if do_kmeans:
@@ -218,23 +237,58 @@ if do_kmeans:
     labels = km.labels_
     centers = km.cluster_centers_
 
-# =============================================================================
-# Spectral Clustering
-# =============================================================================
-
-n_clusters = 4
-do_sc = False
-
-if do_sc:  # problem with SC is that it is very slow with n_clusters>=5
-    sc = SpectralClustering(n_clusters, eigen_solver="arpack", affinity="rbf", n_neighbors=5, assign_labels="discretize", random_state=0)
-
-    sc.fit(values)
-    labels = sc.labels_
-    print("labels shape", labels.shape)
+    clustered_values = centers[labels]
 
 # =============================================================================
-# Spectral Clustering
+# Recalibrate centers to account for raw magnitudes
 # =============================================================================
+
+    base = smoothed_values
+
+    recalibrated_values = np.zeros(clustered_values.shape)
+    for i in range(n_clusters):
+        face_indices = np.nonzero(labels==i)
+        new_vector = np.mean(base[face_indices], axis=0)
+        recalibrated_values[face_indices] = new_vector
+    
+# =============================================================================
+# Spectral Clustering - (deprecated)
+# =============================================================================
+
+# n_clusters = 3
+# do_sc = False
+
+# if do_sc:  # problem with SC is that it is very slow with n_clusters>=5
+#     sc = SpectralClustering(n_clusters, eigen_solver="arpack", affinity="rbf", n_neighbors=5, assign_labels="discretize", random_state=0)
+
+#     sc.fit(values)
+#     labels = sc.labels_
+
+# =============================================================================
+# Calculate magnitudes
+# =============================================================================
+
+base = smoothed_values
+
+magnitudes = np.zeros(mesh.number_of_faces())
+for fkey in mesh.faces():
+    vec = base[fkey, :]
+    magnitudes[fkey] = np.linalg.norm(vec)
+
+# =============================================================================
+# Calculate resulting deviation
+# =============================================================================
+
+base = values
+target = recalibrated_values
+
+deviations = np.zeros(mesh.number_of_faces())
+for fkey in mesh.faces():
+    deviations[fkey] = fabs(cosine_similarity(base[fkey], target[fkey]))
+
+deviations = 1.0 - deviations  # invert results
+
+print("Loss: {}".format(np.sum(deviations)))
 
 # =============================================================================
 # Plotter
@@ -242,7 +296,25 @@ if do_sc:  # problem with SC is that it is very slow with n_clusters>=5
 
 plotter = ClusterPlotter(mesh, figsize=(12, 9))
 plotter.draw_edges(keys=list(mesh.edges_on_boundary()))
-plotter.draw_vector_field(tag, (0, 0, 0), True, 0.05, 1.0)
+
+# plotter.draw_vector_field_array(target, (0, 0, 255), True, 0.04, 1.0)
+# plotter.draw_vector_field_array(base, (0, 0, 0), True, 0.04, 0.5)
+
+# =============================================================================
+# Data to color
+# =============================================================================
+
+dataset = "magnitudes"
+
+data_collection = {
+    "labels": {"values": labels, "cmap": "jet"},
+    "deviations": {"values": deviations, "cmap": "RdPu"},  # also cmap: Grays
+    "cosim": {"values": cosim, "cmap": "RdBu"},  # also cmap: Spectral
+    "magnitudes": {"values": magnitudes, "cmap": "Blues"}
+}
+
+data = data_collection[dataset]["values"]
+cmap = data_collection[dataset]["cmap"]
 
 # =============================================================================
 # Color Faces
@@ -250,21 +322,12 @@ plotter.draw_vector_field(tag, (0, 0, 0), True, 0.05, 1.0)
 
 collection = plotter.draw_faces()
 
-values = labels
+# plt.colormaps()
 
-"""
-Divergent colormaps
-luminance highest at midpoint
-
-RdBu      red, white, blue (ok)
-RdYlBu    red, yellow, blue (ok)
-RdYlGn    red, yellow, green (ok)
-Spectral  red, orange, yellow, green, blue (ok)
-"""
-
-collection.set(array=values, cmap='jet')
-# collection.set_clim(vmin=round(min_cosim, 2), vmax=round(max_cosim, 2))
+collection.set(array=data, cmap=cmap)  # deviations
 colorbar = plotter.figure.colorbar(collection)
+
+# collection.set_clim(vmin=round(min_cosim, 2), vmax=round(max_cosim, 2))
 # ticks = [min_cosim] + colorbar.get_ticks().tolist() + [max_cosim]
 # colorbar.set_ticks([round(t, 2) for t in ticks])
 
