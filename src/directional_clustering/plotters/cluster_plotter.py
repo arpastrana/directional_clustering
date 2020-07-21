388a26c343382e1733b18d2e47df2009a3952102
Rafael Pastrana <pastrana@arch.ethz.ch>
2020-07-20 23:21:06 -0500

    Implemented draw vector field from array

--- src/directional_clustering/plotters/cluster_plotter.py
+++ src/directional_clustering/plotters/cluster_plotter.py
@@ -1,6 +1,7 @@
 from directional_clustering.geometry import contour_polygons
 from directional_clustering.geometry import line_tuple_to_dict
 from directional_clustering.geometry import vector_lines_on_faces
+from directional_clustering.geometry import line_sdl
 
 from compas_plotters import MeshPlotter
 
@@ -24,6 +25,36 @@ class ClusterPlotter(MeshPlotter):
         lines = []
 
         _lines = vector_lines_on_faces(mesh, tag, uniform, scale)
+        _lines = [line for line in map(line_tuple_to_dict, _lines)]
+
+        for line in _lines:
+            line["width"] = width
+            line["color"] = color
+
+        lines.extend(_lines)
+
+        self.draw_lines(lines)
+
+    def draw_vector_field_array(self, field, color, uniform, scale, width=0.5):
+        mesh = self.mesh
+        lines = []
+
+    
+        _lines = []
+
+        rows, cols = field.shape
+        for fkey in range(rows):
+            vector = field[fkey]
+            vector = vector.tolist()
+
+            if uniform:
+                vec_length = scale
+            else:
+                vec_length = length_vector(vector) * scale
+
+            pt = mesh.face_centroid(fkey)
+            _lines.append(line_sdl(pt, vector, vec_length))
+
         _lines = [line for line in map(line_tuple_to_dict, _lines)]
 
         for line in _lines:
