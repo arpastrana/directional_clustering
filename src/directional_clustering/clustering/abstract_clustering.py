d422ca451e390855359e1b0629614d37a8d8bac0
Rafael Pastrana <arpj@princeton.edu>
2020-11-02 21:02:15 -0600

    Created draft ABC for clustering algorithms

---
+++ src/directional_clustering/clustering/abstract_clustering.py
@@ -0,0 +1,24 @@
+from abc import ABC
+from abc import abstractmethod
+
+
+__all__ = [
+    "ClusteringAlgorithm"
+    ]
+
+
+class ClusteringAlgorithm(ABC):
+    """
+    Abstract base class for all clustering algorithms.
+    """
+    def __init__(self):
+        # these may be better off as properties
+        self.loss = None
+        self.centroids = None
+        self.labels = None
+
+    @abstractmethod
+    def cluster(self):
+        """
+        """
+        return
