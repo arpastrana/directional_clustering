4b2e97e8b1cc2ccf6a8ccc04174eda20f06b5cba
isabelmdo <40008482+isabelmdo@users.noreply.github.com>
2020-11-18 16:42:19 -0300

    Renamed test_example to test_geometry and added another line test

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -6,18 +6,18 @@ import directional_clustering
 import pytest
 from directional_clustering.geometry import line_sdl
 
-def test_dummy_fails():
-    string = "hello_world"
-    assert string != "hello"
 
-
-def test_dummy_success():
-    string = "hello_world"
-    assert string == "hello_world"
-
-def test_line_sdl():
+def test_line_sdlOneSide():
     start = [0.0, 1.0, 0.0]
     direction = [1.0, 0.0, 0.0]
     length = 1.0
     ln = line_sdl(start, direction, length, False)
     assert ln == (start, [1.0, 1.0, 0.0])
+
+
+def test_line_sdlBothSides():
+    start = [0.0, 1.0, 0.0]
+    direction = [1.0, 0.0, 0.0]
+    length = 1.0
+    ln = line_sdl(start, direction, length, True)
+    assert ln == ([-1.0, 1.0, 0.0], [1.0, 1.0, 0.0])
