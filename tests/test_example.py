46ce09c61103b175d7cf898766f90bd7bf8bf0e3
isabelmdo <40008482+isabelmdo@users.noreply.github.com>
2020-11-18 16:25:32 -0300

    Added a test for line_sdl

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,7 +4,7 @@
 import os
 import directional_clustering
 import pytest
-
+from directional_clustering.geometry import line_sdl
 
 def test_dummy_fails():
     string = "hello_world"
@@ -15,3 +15,9 @@ def test_dummy_success():
     string = "hello_world"
     assert string == "hello_world"
 
+def test_line_sdl():
+    start = [0.0, 1.0, 0.0]
+    direction = [1.0, 0.0, 0.0]
+    length = 1.0
+    ln = line_sdl(start, direction, length, False)
+    assert ln == (start, [1.0, 1.0, 0.0])
