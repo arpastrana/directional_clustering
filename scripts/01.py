dd9c294d73c98ac1694d06e93c33e253c7acf710
Rafael Pastrana <arpj@princeton.edu>
2020-11-24 15:41:06 -0600

    Added argument descriptions to 01.py

--- scripts/01.py
+++ scripts/01.py
@@ -11,8 +11,6 @@ import numpy as np
 from compas.datastructures import Mesh
 
 # geometry helpers
-from compas.geometry import scale_vector
-from compas.geometry import dot_vectors
 from compas.geometry import subtract_vectors
 from compas.geometry import length_vector_sqrd
 
@@ -62,73 +60,109 @@ vectorfield_tags= [
     "custom_2"   # an arbitrary vector field pointing in the global X direction
     ]
 
-# ==============================================================================
-# Set directory of input and output JSON files
-# ==============================================================================
-
-# Relative path to the JSON file stores the vector fields and the mesh info
-# The JSON files are stored in the data/json_files folder
-# I am working on MacOS, so the format might be slightly different on Windows
-name_in = "perimeter_supported_slab.json"
-name_out = "perimeter_supported_slab_k_5.json"
-
-JSON_IN = os.path.abspath(os.path.join(JSON, name_in))
-JSON_OUT = os.path.abspath(os.path.join(JSON, name_out))
 
 # ==============================================================================
 # Main function: directional_clustering
 # ==============================================================================
 
-def directional_clustering(vectorfield_tag = "m_1", 
-                            align_vectors = True, alignment_ref = [1.0, 0.0, 0.0],
-                            smooth_iters = 10, damping = 0.5,
-                            clustering_name = "variational kmeans", n_clusters = 5, tol = 1e-6, iters = 30,
-                            export_json = False,
-                            draw_faces = True, draw_vector_fields = False):
+def directional_clustering(vectorfield_tag="m_1",
+                           align_vectors=True,
+                           alignment_ref=[1.0, 0.0, 0.0],
+                           smooth_iters=10,
+                           damping=0.5,
+                           clustering_name="variational kmeans",
+                           n_clusters=5,
+                           tol=1e-6,
+                           iters=30,
+                           export_json=False,
+                           draw_faces=True,
+                           draw_vector_fields=False):
+    """
+    Cluster a vector field atop a mesh based on its direction.
+
+    Parameters
+    ----------
+    vectorfield_tag : `str``
+        The name of the vector field to cluster.
+        \nDefaults to "m_1".
+
+    align_vectors : `bool`
+        Flag to align vectors relative to a reference vector.
+        \nDefaults to False.
+
+    alignment_ref : `list` of `float`
+        The reference vector for alignment.
+        \nDefaults to [1.0, 0.0, 0.0].
+
+    smooth_iters : `int`
+        The number iterations of Laplacian smoothing on the vector field.
+        \nIf set to 0, no smoothing will take place.
+        Defaults to 10.
+
+    damping : `float`
+        A value between 0.0 and 1.0 to control the intensity of the smoothing.
+        \nZero technically means no smoothing. One means maximum smoothing.
+        Defaults to 0.5.
+
+    clustering_name : `str`
+        The name of the algorithm to cluster the vector field.
+        \nSupported options are `cosine kmeans` and `variational kmeans`.
+        Defaults to `cosine kmeans`.
+
+    n_clusters : `int`
+        The number of clusters to generate.
+        \nDefaults to 5.
+
+    iters : `int`
+        The number of iterations to run the clustering algorithm for.
+        \nDefaults to 30.
+
+    tol : `float`
+        A small threshold value that marks clustering convergence.
+        \nDefaults to 1e-6.
+
+    export_json: `bool`
+        True to export the vector field and the mesh to JSON file.
+        \nDefaults to False.
+
+    draw_faces : `bool`
+        Colors the faces of the mesh based on the resulting labales of a clustered field.
+        \nDefaults to True.
+
+    draw_vector_fields : `bool`
+        Plots the clustered vector field atop of the input mesh.
+        \nDefaults to False.
+    """
+
     # ==============================================================================
-    # Arguments
-
-    # vector field
-    # vectorfield_tag: the vector field to base the clustering on
-
-    # alignment
-    # align_vectors: the boolean value showing whether to align vectors
-    # alignment_ref: reference vector for alignment
-
-    # smoothing
-    # smooth_iters: how many iterations to run the smoothing for
-    # damping: damping coefficient, a value from 0 to 1
-
-    # clustering
-    # clustering_name: algorithm name
-    # n_clusters: clustering_namenumber of clusters to produce
-    # tol: loss function threshold for early stopping
-    # iters: number of epochs to run kmeans clustering for
-
-    # exporting
-    # export_json: the boolean value showing whether to export mesh to JSON file
-    
-    # plotter flags
-    # draw_faces = True
-    # draw_vector_fields = False
+    # Set directory of input and output JSON files
     # ==============================================================================
+
+    # Relative path to the JSON file stores the vector fields and the mesh info
+    # The JSON files are stored in the data/json_files folder
+
+    name_in = "perimeter_supported_slab.json"
+    name_out = "perimeter_supported_slab_k_5.json"
+
+    JSON_IN = os.path.abspath(os.path.join(JSON, name_in))
+    JSON_OUT = os.path.abspath(os.path.join(JSON, name_out))
 
     # ==============================================================================
     # Import a COMPAS mesh
     # ==============================================================================
     mesh = Mesh.from_json(JSON_IN)
-    
+
     # ==============================================================================
     # Extract vector field from COMPAS mesh for clustering
     # ==============================================================================
-    
+
     # Extract a vector field from the faces of a mesh
     vectors = VectorField.from_mesh_faces(mesh, vectorfield_tag)
-    
+
     # ==============================================================================
     # Align vector field to a reference vector
     # ==============================================================================
-    
+
     # the output of the FEA creates vector fields that are oddly oriented.
     # Eventually, what we want is to create "lines" from this vector
     # field that can be materialized into reinforcement bars, beams, or pipes which
@@ -144,14 +178,14 @@ def directional_clustering(vectorfield_tag = "m_1",
     # how to pick the reference vector is arbitrary ("user-defined") and something
     # where there's more work to be done on. in the meantime, i've used the global
     # x and global y vectors as references, which have worked ok for my purposes.
-    
+
     if align_vectors:
         align_vector_field(vectors, alignment_ref)
-    
+
     # ==============================================================================
     # Apply smoothing to the vector field
     # ==============================================================================
-    
+
     # moreover, depending on the quality of the initial mesh, the FEA-produced
     # vector field will be very noisy, especially around "singularities".
     # this means there can be drastic orientation jumps/flips between two vectors
@@ -166,14 +200,14 @@ def directional_clustering(vectorfield_tag = "m_1",
     # too much smoothing however, will actually distort the initial field to the
     # point that is not longer "representing" the original vector field
     # so smoothing is something to use with care
-    
+
     if smooth_iters:
         smoothen_vector_field(vectors, mesh.face_adjacency(), smooth_iters, damping)
-    
+
     # ==============================================================================
     # Do K-means Clustering
     # ==============================================================================
-    
+
     # now we need to put the vector field into numpy arrays to carry out clustering
     # current limitation: at the moment this only works in planar 2d meshes!
     # other clustering methods, like variational clustering, can help working
@@ -181,7 +215,7 @@ def directional_clustering(vectorfield_tag = "m_1",
     # other ideas relate to actually "reparametrizing" (squishing) a 3d mesh into a
     # 2d mesh, carrying out clustering directly in 2d, and then reconstructing
     # the results back into the 3d mesh ("reparametrizing it back")
-    
+
     # One of the key differences of this work is that use cosine distance as
     # basis metric for clustering. this is in constrast to numpy/scipy
     # whose implementations, as far as I remember support other types of distances
@@ -189,7 +223,7 @@ def directional_clustering(vectorfield_tag = "m_1",
     # work for the vector fields used here, but maybe this has changed now.
     # in any case, this "limitation" led me to write our own version of kmeans
     # which can do clustering based either on cosine or euclidean similarity
-    
+
     # kmeans is sensitive to initialization
     # there are a bunch of approaches that go around it, like kmeans++
     # i use here another heuristic which iteratively to find the initial seeds
@@ -197,12 +231,12 @@ def directional_clustering(vectorfield_tag = "m_1",
     # vector which is the "most distant" at a given iteration using kmeans itself
     # These seeds will be used later on as input to start the final kmeans run.
     print("Clustering started...")
-    
+
     # Create an instance of a clustering algorithm
-    
+
     clustering_algo = ClusteringFactory.create(clustering_name)
     clusterer = clustering_algo(mesh, vectors, n_clusters, iters, tol)
-    
+
     # do kmeans clustering
     # labels contains the cluster index assigned to every vector in the vector field
     # centers contains the centroid of every cluster (the average of all vectors in
@@ -210,24 +244,24 @@ def directional_clustering(vectorfield_tag = "m_1",
     # the loss is simply the mean squared error of the cosine distance between
     # every vector and the centroid of the cluster it is assigned to
     # the goal of kmeans is to minimize this loss function
-    
+
     clusterer.cluster()
-    
-    print("Loss Clustering: {}".format(clusterer.error))
+
+    print("Loss Clustering: {}".format(clusterer.loss))
     print("Clustering ended!")
-    
+
     # make an array with the assigned labels of all vectors
     clustered_field = clusterer.clustered_field
     labels = clusterer.labels
-    
+
     # ==============================================================================
     # Compute mean squared error "loss" of clustering w.r.t. original vector field
     # ==============================================================================
-    
+
     # probably would be better to encapsulate this in a function or in a Loss object
     # clustering_error = MeanSquaredError(vector_field, clustered_field)
     # clustering_error = MeanAbsoluteError(vector_field, clustered_field)
-    
+
     errors = np.zeros(mesh.number_of_faces())
     for fkey in mesh.faces():
         # for every face compute difference between clustering output and
@@ -236,53 +270,53 @@ def directional_clustering(vectorfield_tag = "m_1",
         difference_vector = subtract_vectors(clustered_field.vector(fkey),
                                              vectors.vector(fkey))
         errors[fkey] = length_vector_sqrd(difference_vector)
-    
+
     mse = np.mean(errors)
-    
+
     print("Clustered Field MSE: {}".format(mse))
-    
+
     # ==============================================================================
     # Assign clusters back to COMPAS mesh
     # ==============================================================================
-    
+
     # this is for visualization and exporting purposes
     attr_name = vectorfield_tag + "_k_{}".format(n_clusters)  # name for storage
-    
+
     # iterate over the faces of the COMPAS mesh
     for fkey in mesh.faces():
         c_vector = clustered_field.vector(fkey)  # convert numpy array to list
         c_label = labels[fkey]
-    
+
         # store clustered vector in COMPAS mesh as a face attribute
         mesh.face_attribute(key=fkey, name=attr_name, value=c_vector)
         mesh.face_attribute(key=fkey, name="cluster", value=c_label)
-    
+
     # ==============================================================================
     # Export new JSON file for further processing
     # ==============================================================================
-    
+
     if export_json:
         mesh.to_json(JSON_OUT)
         print("Exported mesh to: {}".format(JSON_OUT))
-    
+
     # =============================================================================
     # Plot stuff
     # =============================================================================
-    
+
     # there is a lot of potential work to do for visualization!
     # below there is the simplest snippet, but you can see more stuff 
     # in the scripts/visualization folder
-    
+
     # ClusterPlotter is a custom wrapper around a COMPAS MeshPlotter
     # the COMPAS MeshPlotter is built atop of pure Matplotlib (which is crazy)
     # what is different here is that I extended the plotter so that it can plot 
     # vector fields directly as little lines via
     # ClusterPlotter.draw_vector_field_array()
     plotter = ClusterPlotter(mesh, figsize=(12, 9))
-    
+
     # draw only the boundary edges of the COMPAS Mesh
     plotter.draw_edges(keys=list(mesh.edges_on_boundary()))
-    
+
     if draw_faces:
         # color up the faces of the COMPAS mesh according to their cluster
         # make a dictionary with all labels
