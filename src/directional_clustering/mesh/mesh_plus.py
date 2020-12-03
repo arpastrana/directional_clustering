f9ee20a8516635e779094d9ca4c318d89b9fbedc
hhhhuiyuan <71137876+hhhhuiyuan@users.noreply.github.com>
2020-12-03 10:26:42 -0500

    update MeshPlus with extra methods discussed before

--- src/directional_clustering/mesh/mesh_plus.py
+++ src/directional_clustering/mesh/mesh_plus.py
@@ -1,9 +1,6 @@
 from compas.datastructures import Mesh
 from directional_clustering.fields import VectorField
-<<<<<<< Updated upstream
-=======
 import os
->>>>>>> Stashed changes
 
 __all__ = ["MeshPlus"]
 
@@ -11,52 +8,64 @@ class MeshPlus(Mesh):
     """
     Extand the original Mesh class with extra methods.
     """
-<<<<<<< Updated upstream
-    def get_vectorfield(self, name, vector_field = None):
-=======
     def vectorfield(self, name, vector_field = None):
->>>>>>> Stashed changes
         """
         Get or set the vector field under some certain attribute from or in a mesh.
         """
         if vector_field is None:
             vector_field = VectorField()
             for fkey in self.faces():
-                vector_field.add_vector(fkey, self.face_attribute(fkey, name))
+                try:
+                    value = self.face_attribute(fkey, name)
+                    if type(value) is list:
+                        vector_field.add_vector(fkey, value)
+                    else:
+                        raise ValueError
+                except ValueError:
+                    return None        #the attribute doesn't exist or it's not a vectorfield
             return vector_field
-<<<<<<< Updated upstream
-        else:
-=======
         else: 
             assert vector_field.size() == self.number_of_faces(), "The vector field to add is incompatible with the mesh"
->>>>>>> Stashed changes
             for vkey in vector_field.keys():
-               self.face_attribute(vkey, name, vector_field.vector(vkey))
+                self.face_attribute(vkey, name, vector_field.vector(vkey))
 
 
-<<<<<<< Updated upstream
-    def vectorfields(self):
-        """
-        Search for attributes of all supported vector fields in a mesh.
-        """
-        
-=======
     def all_vectorfields(self):
         """
         Search for attributes of all supported vector fields in a mesh.
         """
-        
+        fkey = self.get_any_face()
+
+        attr_view = self.face_attributes(fkey)
+        attr_default = list(attr_view.keys()) 
+        attr_view.custom_only = True
+        attr_costom = list(attr_view.keys())
+        attr = attr_default + attr_costom
+
+        attr_vectorfield = []
+        for name in attr:
+            if self.vectorfield(name) is not None: 
+                attr_vectorfield.append(name)
+
+        return attr_vectorfield
+
+
+
 if __name__ == "__main__":
     from directional_clustering import JSON
     name_in = "perimeter_supported_slab.json"
 
     JSON_IN = os.path.abspath(os.path.join(JSON, name_in))
-    mesh = Mesh.from_json(JSON_IN)
-    
-    #face = mesh.get_any_face()
-    #print(face)
-    attr2 = mesh.face_attributes(885, "m_1")
-    print(attr2)
+    mesh = MeshPlus.from_json(JSON_IN)
+
+    attr = mesh.all_vectorfields()
+    print(attr)
+
+    fkey = mesh.get_any_face()
+    for name in attr :
+        value = mesh.face_attribute(fkey, name)
+        print(value)
     
 
->>>>>>> Stashed changes
+    
+
