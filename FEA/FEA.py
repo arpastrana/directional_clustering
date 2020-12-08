dc3038330ab099bd038df3a6e7bb15de8a8dab69
Alexandros101 <51721592+Alexandros101@users.noreply.github.com>
2020-12-08 02:05:41 +0200

    Update FEA.py

--- FEA/FEA.py
+++ FEA/FEA.py
@@ -1,3 +1,6 @@
+##Working with example from SfePy:
+##Cimrman, R., Lukeš, V., Rohan, E., 2019. Multiscale finite element calculations in Python using SfePy.
+##Adv Comput Math. https://doi.org/10.1007/s10444-019-09666-0
 ##Importing all the packages
 
 from sfepy import data_dir
@@ -20,10 +23,12 @@ eps = 1e-8 * (max_x - min_x)
 omega = domain.create_region('Omega', 'all')
 gamma1 = domain.create_region('Gamma1','vertices in x < %.10f' % (min_x + eps),'facet')
 gamma2 = domain.create_region('Gamma2','vertices in x > %.10f' % (max_x - eps),'facet')
-field = Field.from_args('fu', nm.float64, 'vector', omega, approx_order=2)
+field = Field.from_args('fu', np.float64, 'vector', omega, approx_order=2)
 
 u = FieldVariable('u', 'unknown', field)
 v = FieldVariable('v', 'test', field, primary_var_name='u')
+
+##Material specifications
 m = Material('m', D=stiffness_from_lame(dim=2, lam=1.0, mu=1.0))
 f = Material('f', val=[[0.02], [0.01]])
 integral = Integral('i', order=3)
@@ -33,6 +38,7 @@ eq = Equation('balance', t1 + t2)
 eqs = Equations([eq])
 fix_u = EssentialBC('fix_u', gamma1, {'u.all' : 0.0})
 
+
 def shift_u_fun(ts, coors, bc=None, problem=None, shift=0.0):
     val = shift * coors[:,1]**2
     return val
@@ -40,6 +46,7 @@ def shift_u_fun(ts, coors, bc=None, problem=None, shift=0.0):
 bc_fun = Function('shift_u_fun', shift_u_fun,extra_args={'shift' : 0.01})
 shift_u = EssentialBC('shift_u', gamma2, {'u.0' : bc_fun})
 
+#Specifying problem. Which in our case is elasticity
 ls = ScipyDirect({})
 nls_status = IndexedStruct()
 nls = Newton({}, lin_solver=ls, status=nls_status)
