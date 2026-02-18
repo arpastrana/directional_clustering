4af6cfb55344d58bd2e3d7f4099e54bed551b646
Rafael Pastrana <arpj@princeton.edu>
2026-02-17 19:15:19 -0500

    Select a subset of faces at random to plot

--- src/directional_clustering/plotters/mesh_plotter.py
+++ src/directional_clustering/plotters/mesh_plotter.py
@@ -1,9 +1,10 @@
+import numpy as np
+
 from compas.geometry import length_vector
 
 from compas_plotters import MeshPlotter
 
 from directional_clustering.plotters import line_sdl
-from directional_clustering.plotters import line_tuple_to_dict
 
 
 __all__ = ["MeshPlusPlotter"]
@@ -17,12 +18,24 @@ class MeshPlusPlotter(MeshPlotter):
         super(MeshPlusPlotter, self).__init__(*args, **kwargs)
         self.name = "MeshPlus Plotter"
 
-    def draw_vector_field(self, field, color, uniform, scale, width=0.5, as_arrows=False):
+    def draw_vector_field(self,
+                          field,
+                          color,
+                          uniform,
+                          scale,
+                          width=0.5,
+                          ratio=1.0,
+                          seed=None,
+                          as_arrows=False):
         """
         Draws a vector field on a mesh.
 
         It assumes the field and the mesh faces have the same keys.
         """
+        assert ratio > 0.0
+
+        np.random.seed(seed)
+
         lines = []
         mesh = self.mesh
 
@@ -30,7 +43,13 @@ class MeshPlusPlotter(MeshPlotter):
         if as_arrows:
             both_sides = False
 
-        for fkey in range(field.size()):
+        # TODO: we assume the fkeys are ordered ints in the range [0, field.size()]
+        fkeys = list(range(field.size()))
+
+        if ratio < 1.0:
+            fkeys = np.random.choice(fkeys, size=int(field.size() * ratio), replace=False)
+
+        for fkey in fkeys:
             line = {}
             vector = field[fkey]
             length = scale
