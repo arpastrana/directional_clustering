86b16a7dc3846c8812a6700e13b2ecba6e34fa26
Rafael Pastrana <arpj@princeton.edu>
2026-02-18 17:19:25 -0500

    [Transforms] More work on merge.py

--- src/directional_clustering/transformations/merge.py
+++ src/directional_clustering/transformations/merge.py
@@ -1,5 +1,6 @@
-from compas.datastructures import Mesh
 from compas.topology import connected_components
+
+from directional_clustering.fields import VectorField
 
 
 # __all__ = [
@@ -9,120 +10,7 @@ from compas.topology import connected_components
 #     ]
 
 
-# def explode_mesh(mesh, min_num_faces=1):
-#     """
-#     Explode a mesh into all its disjoint meshes using the connected components of the face adjacency.
-
-#     Parameters
-#     ----------
-#     mesh : `compas.datastructures.Mesh`
-#         The mesh to explode.
-#     min_num_faces : `int`, optional
-#         The minimum number of faces to consider a connected component.
-#         Defaults to `1`.
-
-#     Returns
-#     -------
-#     exploded_meshes : `list` of `compas.datastructures.Mesh`
-#         The exploded meshes.
-#     """
-#     face_adjacency = {fkey: mesh.face_neighbors(fkey) for fkey in mesh.faces()}
-#     parts = [set(part) for part in connected_components(face_adjacency)]
-
-#     cls = type(mesh)
-#     exploded_meshes = []
-#     for part in parts:
-
-#         part = set(part)
-#         if len(part) < min_num_faces:
-#             continue
-
-#         vertex_keys = list(set([vkey for fkey in part for vkey in mesh.face_vertices(fkey)]))
-#         vertices = [mesh.vertex_coordinates(vkey) for vkey in vertex_keys]
-#         key_to_index = {vkey: i for i, vkey in enumerate(vertex_keys)}
-#         faces = [[key_to_index[vkey] for vkey in mesh.face_vertices(fkey)] for fkey in part]
-#         exploded_meshes.append(cls.from_vertices_and_faces(vertices, faces))
-
-#     return exploded_meshes
-
-
-# def compute_k_meshes(mesh, cluster_keys, face_labels):
-#     """
-#     Generates a mesh per directional cluster by grouping faces based on their cluster labels.
-
-#     Parameters
-#     ----------
-#     mesh : `directional_clustering.mesh.MeshPlus`
-#         The mesh to compute the k-meshes from.
-#     cluster_keys : `list` of `int`
-#         The keys of the clusters to compute the meshes for.
-#     face_labels : `dict` of `int` to `int`
-#         A map from a face key to its cluster label.
-
-#     Returns
-#     -------
-#     k_meshes : `dict` of `int` to `directional_clustering.mesh.MeshPlus`
-#         A map from a cluster key to its mesh.
-#     """
-#     k_meshes = {}
-
-#     for ckey in cluster_keys:
-#         k_fkeys = [fkey for fkey, c in face_labels.items() if c == ckey]
-#         k_polygons = [mesh.face_coordinates(fkey) for fkey in k_fkeys]
-#         k_mesh = Mesh.from_polygons(k_polygons)
-#         k_meshes[ckey] = k_mesh
-
-#     return k_meshes
-
-
-# def compute_k_meshes_disjoint(mesh, cluster_keys, face_labels):
-#     """
-#     Generates a list of disjoint meshes per directional cluster.
-#     The meshes in a cluster are obtained by based on the connected components of the face adjacency.
-
-#     Parameters
-#     ----------
-#     mesh : `directional_clustering.mesh.MeshPlus`
-#         The mesh to compute the k-meshes from.
-#     cluster_keys : `list` of `int`
-#         The keys of the clusters to compute the meshes for.
-#     face_labels : `dict` of `int` to `int`
-#         A map from a face key to its cluster label.
-
-#     Returns
-#     -------
-#     k_meshes_disjoint : `dict` of `int` to `list` of `compas.datastructures.Mesh`
-#         A map from a cluster key to its list of disjoint meshes.
-#     """
-#     k_meshes_disjoint = {}
-#     k_meshes = compute_k_meshes(mesh, cluster_keys, face_labels)
-#     for ckey, k_mesh in k_meshes.items():
-#         k_meshes_disjoint[ckey] = explode_mesh(k_mesh)
-
-#     return k_meshes_disjoint
-
-
-# def merge_clusters(mesh, cluster_keys, face_labels, min_area_ratio=0.05):
-#     """
-#     Merges small clusters into their smallest neighboring cluster if their area is smaller than the given threshold.
-
-#     Parameters
-#     ----------
-#     mesh : `directional_clustering.mesh.MeshPlus`
-#         The mesh to merge the clusters of.
-#     min_area_ratio : `float`
-#         The minimum area ratio of a disjoint cluster mesh to be considered for merging.
-#         Defaults to `0.05`.
-
-#     Returns
-#     -------
-#     """
-#     mesh = mesh.copy()
-#     k_meshes_disjoint = compute_k_meshes_disjoint(mesh, cluster_keys, face_labels)
-
-#     return mesh
-
-def compute_face_adjacency_per_cluster_from_mesh(mesh):
+def compute_face_adjacency_per_cluster(mesh):
     """
     Computes the adjacency of the faces of a mesh excluding faces that belong to a different cluster.
 
@@ -135,6 +23,10 @@ def compute_face_adjacency_per_cluster_from_mesh(mesh):
     -------
     face_adjacency : `dict` of `int` to `list` of `int`
         A map from a face key to the face keys of its neighbors within the same cluster.
+
+    Notes
+    -----
+    The mesh faces must have a cluster label stored as an attribute.
     """
     # Compute face adjacencies
     face_adjacency = {}
@@ -148,6 +40,228 @@ def compute_face_adjacency_per_cluster_from_mesh(mesh):
 
     return face_adjacency
 
+
+def generate_connected_parts(adjacency):
+    """
+    Generates connected components from an adjacency.
+
+    Parameters
+    ----------
+    adjacency : `dict` of `int` to `list` of `int`
+        A map from a element keys to the neighbors of each element key.
+
+    Returns
+    -------
+    parts : `dict` of `int` to `set` of `int`
+        A map from a part key to the element keys of the part.
+        Each part is a connected component of the adjacency.
+    """
+    parts = {pkey: set(part) for pkey, part in enumerate(connected_components(adjacency))}
+
+    return parts
+
+
+def generate_connected_parts_adjacency(mesh, parts):
+    """
+    Generates the adjacency of the connected components of a mesh.
+
+    Parameters
+    ----------
+    adjacency : `dict` of `int` to `list` of `int`
+        A map from a element keys to the neighbors of each element key.
+
+    Returns
+    -------
+    connected_components_adjacency : `dict` of `int` to `set` of `int`
+        A map from a part key to the part keys of its neighbors.
+    """
+    # Map faces to parts
+    fkeys_to_pkeys = {}
+    for fkey in mesh.faces():
+        for pkey, part in parts.items():
+            if fkey in part:
+                fkeys_to_pkeys[fkey] = pkey
+                break
+
+    # Compute part adjacencies
+    part_adjacency = {pkey: set() for pkey in parts.keys()}
+    for fkey, pkey in fkeys_to_pkeys.items():
+        for okey in mesh.face_neighbors(fkey):
+            qkey = fkeys_to_pkeys[okey]
+            if pkey != qkey:
+                part_adjacency[pkey].add(qkey)
+
+    # Check that each part has at least one neighbor. No islands allowed.
+    for pkey, nbrs in part_adjacency.items():
+        assert len(nbrs) > 0, "Part has no neighbors"
+
+    return part_adjacency
+
+
+def compute_cluster_labels_from_parts(mesh, parts):
+    """
+    Computes the cluster labels from the parts.
+
+    Parameters
+    ----------
+    mesh : `directional_clustering.mesh.MeshPlus`
+        The mesh to compute the cluster labels from.
+    parts : `dict` of `int` to `set` of `int`
+        A map from a part key to the face keys of the part.
+
+    Returns
+    -------
+    pkeys_to_labels : `dict` of `int` to `int`
+        A map from a part key to the cluster label.
+    """
+    pkeys_to_labels = {}
+    for pkey, part in parts.items():
+        part_labels = set(mesh.face_attribute(fkey, "cluster") for fkey in part)
+        assert len(part_labels) == 1, "Part contains faces from different clusters"
+        pkeys_to_labels[pkey] = part_labels.pop()
+
+    return pkeys_to_labels
+
+
+def _compute_faces_area(mesh, part):
+    """
+    """
+    return sum([mesh.face_area(fkey) for fkey in part])
+
+
+def merge_parts(mesh, parts, part_adjacency, min_area_ratio, max_iters=100):
+    """
+    Merges parts of a mesh based on their area.
+
+    Parameters
+    ----------
+    mesh : `directional_clustering.mesh.MeshPlus`
+        The mesh to merge the parts of.
+    parts : `dict` of `int` to `set` of `int`
+        A map from a part key to the face keys of the part.
+    part_adjacency : `dict` of `int` to `set` of `int`
+        A map from a part key to the part keys of its neighbors.
+    min_area_ratio : `float`
+        The minimum area ratio of a part to merge it with its smallest neighbor.
+    max_iters : `int`, optional
+        The maximum number of iterations to perform. Defaults to 100.
+
+    Returns
+    -------
+    new_parts : `dict` of `int` to `set` of `int`
+        A map from a part key to the face keys of the part.
+    """
+    total_area = mesh.area()
+    assert min_area_ratio < 1.0, "Minimum area ratio must be less than 1.0"
+    min_area = min_area_ratio * total_area
+
+    parts = {k: v for k, v in parts.items()}
+    part_adjacency = {k: v for k, v in part_adjacency.items()}
+
+    for _ in range(max_iters):
+        print(f"***Looping starts with {len(parts)} parts***")
+
+        pkeys_areas = {pkey: _compute_faces_area(mesh, parts[pkey]) for pkey in parts.keys()}
+        pkeys = [pkey for pkey in parts.keys() if pkeys_areas[pkey] <= min_area]
+
+        if len(pkeys) == 0:
+            break
+
+        sorted_pkeys = sorted(pkeys, key=lambda x: pkeys_areas[x])
+
+        for pkey in sorted_pkeys:
+            print(f"Processing part {pkey}")
+
+            # Find smallest neighbor
+            nbrs = part_adjacency[pkey]
+            smallest_nbr = min(nbrs, key=lambda x: pkeys_areas[x])
+            print(f"\tMerging part {pkey} with area {pkeys_areas[pkey]} into smallest neighbor {smallest_nbr}")
+
+            # Merge part faces into smallest neighbor's faces
+            part = parts[pkey]
+            parts[smallest_nbr].update(part)
+            print(f"\tUpdated part {smallest_nbr} with part {part}")
+            for _pkey in part_adjacency[pkey]:
+                if _pkey != smallest_nbr:
+                    part_adjacency[smallest_nbr].add(_pkey)
+            print(f"\tUpdated part adjacency of {smallest_nbr} with adjacency {part_adjacency[pkey]}")
+
+            # Recompute part adjacency
+            for nbr in nbrs:
+                part_adjacency[nbr].remove(pkey)
+                if nbr != smallest_nbr:
+                    part_adjacency[nbr].add(smallest_nbr)
+
+            # Delete part
+            del parts[pkey]
+            del part_adjacency[pkey]
+            break
+
+        # print(f"Any changes: {any_changes}")
+        print("\tIntermediate part adjacencies:")
+        for pkey, nbrs in part_adjacency.items():
+            print(f"\t{pkey}: {nbrs}")
+        print("***Looping ends***")
+
+    return parts
+
+
+def compute_face_clusters_from_parts(parts, pkeys_to_labels):
+    """
+    Computes the cluster labels of the faces of a mesh from the parts.
+
+    Parameters
+    ----------
+    parts : `dict` of `int` to `set` of `int`
+        A map from a part key to the face keys of the part.
+    pkeys_to_labels : `dict` of `int` to `int`
+        A map from a part key to the cluster label.
+
+    Returns
+    -------
+    labels : `dict` of `int` to `int`
+        A map from a face key to the cluster label.
+    """
+    labels = {}
+    for pkey, part in parts.items():
+        label = pkeys_to_labels[pkey]
+        for fkey in part:
+            labels[fkey] = label
+
+    return labels
+
+
+def compute_vector_field_from_clusters(labels, cluster_centroids):
+    """
+    Computes the vector field from the clusters.
+    """
+    vector_field = VectorField()
+    for fkey, label in labels.items():
+        vector = cluster_centroids[label]
+        vector_field.add_vector(fkey, vector)
+    return vector_field
+
+
+def merge_clusters(mesh, min_area_ratio, max_iters=100):
+    """
+    Merges clusters of a mesh in geometry space based on the cluster'sarea.
+    """
+    face_adjacency = compute_face_adjacency_per_cluster(mesh)
+    parts = generate_connected_parts(face_adjacency)
+    part_adjacency = generate_connected_parts_adjacency(mesh, parts)
+    pkeys_to_labels = compute_cluster_labels_from_parts(mesh, parts)
+
+    new_parts = merge_parts(mesh, parts, part_adjacency, min_area_ratio, max_iters)
+
+    # Merged data
+    labels = compute_face_clusters_from_parts(new_parts, pkeys_to_labels)
+    # vector_field = compute_vector_field_from_clusters(labels, clustered_field)
+
+    # TODO: Check all faces are included in the new parts
+    # TODO: Check sum of cluster meshes equals total area of mesh
+    # TODO: Check sum of cluster disjoint meshes equals total area of mesh
+
+    return labels  # , vector_field
 
 
 if __name__ == "__main__":
@@ -179,158 +293,20 @@ if __name__ == "__main__":
         for fkey in fkeys:
             mesh.face_attribute(fkey, "cluster", ckey)
 
-    # Check that all faces have a cluster label
-    for fkey in mesh.faces():
-        assert mesh.face_attribute(fkey, "cluster") is not None
+    # Data extraction
+    labels = mesh.cluster_labels("cluster")
+    n_clusters = len(clusters)
 
     # Check sum of cluster areas equals total area of mesh
     total_area = mesh.area()
     cluster_areas = {ckey: sum([mesh.face_area(fkey) for fkey in fkeys]) for ckey, fkeys in clusters.items()}
     assert sum(cluster_areas.values()) == total_area, "Sum of cluster areas does not equal total area of mesh"
 
-    labels = mesh.cluster_labels("cluster")
-    n_clusters = len(clusters)
-
-    # Check sum of cluster meshes equals total area of mesh
-    k_meshes = compute_k_meshes(mesh, clusters.keys(), labels)
-    assert sum([k_mesh.area() for k_mesh in k_meshes.values()]) == total_area, "Sum of cluster meshes does not equal total area of mesh"
-
-    # Check sum of cluster disjoint meshes equals total area of mesh
-    k_meshes_disjoint = compute_k_meshes_disjoint(mesh, clusters.keys(), labels)
-    areas = []
-    for ckey, k_meshes in k_meshes_disjoint.items():
-        for k_mesh in k_meshes:
-            areas.append(k_mesh.area())
-    assert sum(areas) == total_area, "Sum of cluster disjoint meshes does not equal total area of mesh"
-
-    # Merge clusters
-
-    # Compute face adjacencies
-    face_adjacency_inside = {}
-    for fkey in mesh.faces():
-        cluster_label = mesh.face_attribute(fkey, "cluster")
-        inside = []
-        for okey in mesh.face_neighbors(fkey):
-            if mesh.face_attribute(okey, "cluster") == cluster_label:
-                inside.append(okey)
-        face_adjacency_inside[fkey] = inside
-
-    # Find connected components per cluster
-    parts = {pkey: set(part) for pkey, part in enumerate(connected_components(face_adjacency_inside))}
-    assert len(parts) == len(parts_truth), "Number of parts does not match number of parts truth"
-    assert all(part in parts_truth for part in parts.values()), "Parts do not match parts truth"
-
-    # Map parts to cluster labels
-    pkeys_to_labels = {}
-    for pkey, part in parts.items():
-        part_labels = set(mesh.face_attribute(fkey, "cluster") for fkey in part)
-        assert len(part_labels) == 1, "Part contains faces from different clusters"
-        pkeys_to_labels[pkey] = part_labels.pop()
-
-    # Map faces to parts
-    fkeys_to_pkeys = {}
-    for fkey in mesh.faces():
-        for pkey, part in parts.items():
-            if fkey in part:
-                fkeys_to_pkeys[fkey] = pkey
-                break
-
-    # Compute part adjacencies
-    part_adjacency = {pkey: set() for pkey in parts.keys()}
-    for fkey, pkey in fkeys_to_pkeys.items():
-        for okey in mesh.face_neighbors(fkey):
-            qkey = fkeys_to_pkeys[okey]
-            if pkey != qkey:
-                part_adjacency[pkey].add(qkey)
-
-    for pkey, nbrs in part_adjacency.items():
-        assert len(nbrs) > 0, "Part has no neighbors"
-
-    # Updated parts
-    print("Initial parts:")
-    for pkey, part in parts.items():
-        print(pkey, part)
-
-    print("Initial part adjacencies:")
-    for pkey, nbrs in part_adjacency.items():
-        print(pkey, nbrs)
-
     # Merging?
-    def polygons_area(mesh, part):
-        return sum([mesh.face_area(fkey) for fkey in part])
-
-    min_area_ratio = 3 / 25.0
-    total_area = mesh.area()
-    assert min_area_ratio < 1.0, "Minimum area ratio must be less than 1.0"
-    min_area = min_area_ratio * total_area
-
-    # Probably loop starts here
-    # small_parts_exist = True
-    num_iters = 100
-    for i in range(num_iters):
-        print(f"***Looping starts with {len(parts)} parts***")
-
-        pkeys_areas = {pkey: polygons_area(mesh, parts[pkey]) for pkey in parts.keys()}
-        pkeys = [pkey for pkey in parts.keys() if pkeys_areas[pkey] <= min_area]
-
-        if len(pkeys) == 0:
-            break
-
-        sorted_pkeys = sorted(pkeys, key=lambda x: pkeys_areas[x])
-
-        for pkey in sorted_pkeys:
-            print(f"Processing part {pkey}")
-
-            # Find smallest neighbor
-            nbrs = part_adjacency[pkey]
-            smallest_nbr = min(nbrs, key=lambda x: pkeys_areas[x])
-            print(f"\tMerging part {pkey} with area {pkeys_areas[pkey]} into smallest neighbor {smallest_nbr}")
-
-            # Merge part faces into smallest neighbor's faces
-            part = parts[pkey]
-            parts[smallest_nbr].update(part)
-            print(f"\tUpdated part {smallest_nbr} with part {part}")
-            for _pkey in part_adjacency[pkey]:
-                if _pkey != smallest_nbr:
-                    part_adjacency[smallest_nbr].add(_pkey)
-            print(f"\tUpdated part adjacency of {smallest_nbr} with adjacency {part_adjacency[pkey]}")
-
-            # Recompute part adjacency
-            # breakpoint()
-            for nbr in nbrs:
-                part_adjacency[nbr].remove(pkey)
-                if nbr != smallest_nbr:
-                    part_adjacency[nbr].add(smallest_nbr)
-
-            # Delete part
-            del parts[pkey]
-            del part_adjacency[pkey]
-            break
-
-        # print(f"Any changes: {any_changes}")
-        print("\tIntermediate part adjacencies:")
-        for pkey, nbrs in part_adjacency.items():
-            print(f"\t{pkey}: {nbrs}")
-        print("***Looping ends***")
-
-    # Updated parts
-    print(f"\nNew number of parts: {len(parts)}")
-    print("Updated parts:")
-    for pkey, part in parts.items():
-        print(pkey, part)
-
-    print("Updated part adjacencies:")
-    for pkey, nbrs in part_adjacency.items():
-        print(pkey, nbrs)
-
-    # Update face labels
-    labels_ = {}
-    for pkey, part in parts.items():
-        label = pkeys_to_labels[pkey]
-        for fkey in part:
-            labels_[fkey] = label
-    labels = labels_
-    # mesh.cluster_labels("cluster", labels)
+    # TODO: Reduce dependency on mesh, input labels and vector field instead
+    # Do not assume the mesh has a cluster attribute and a vector field attribute
+    min_area_ratio = 2 / 25.0
+    labels = merge_clusters(mesh, min_area_ratio)
 
     # Visualization
     cmap = plt.cm.get_cmap("rainbow", n_clusters)
